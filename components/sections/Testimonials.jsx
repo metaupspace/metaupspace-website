c022109b70829ce1d0ddcb2f5b5fa0e0e9c38fdd
Nitin Billa <nitinbilla10@gmail.com>
2024-08-18 15:58:54 +0530

    Update Testimonials.jsx

--- components/sections/Testimonials.jsx
+++ components/sections/Testimonials.jsx
@@ -1,8 +1,46 @@
+'use client'
+
 import React from "react";
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+
+
+const testimonials = [
+  {
+    "name": "Ashutosh Pratap Singh",
+    "designation": "Founder, Technical Sapien (800K+ Followers)",
+    "description": "I am deeply grateful for MetaUpSpace's exceptional service for AI development & community management services. Your team not only expertly handles our vast 500,000+ member community (India's biggest!), but also impresses with your dedication and commitment. Your innovative solutions have been invaluable. I highly recommend MetaUpSpace for anyone seeking to build a product."
+  },
+  {
+    "name": "Samarth Mahabia",
+    "designation": "Youtuber, Barefox (150k+ Subscribers)",
+    "description": "Finding MetaUpSpace was a game-changer! Their editing services and content strategy have propelled my channel to new heights. Their professionalism, knowledge, and willingness to go the extra mile are outstanding. If you're looking for a trustworthy partner in your content creation journey, MetaUpSpace is the answer. Thanks for being the best!"
+  },
+  {
+    "name": "Harsh Kothari",
+    "designation": "Product Designer, Unacademy",
+    "description": "MetaUpSpace stands out for its dedication to cutting-edge Web 3 technology. Witnessing their passion for innovation has been awe-inspiring. Their top-notch web development, smart contract development, and AI chatbot solutions make them a reliable tech partner. I have no doubt that MetaUpSpace will continue to deliver exceptional results."
+  },
+  {
+    "name": "Shivay Lamba",
+    "designation": "Founder, Darkhorse",
+    "description": "I have had the pleasure of working with the team at MetaUpSpace for our company Darkhorse. They have provided us with excellent support for the development of our web application. They have been a valuable member of our team and I would highly recommend their technical expertise to anyone looking for reliable and high-quality integration support."
+  }
+]
+
+
+
 
 function Testimonials() {
+
+
+
+
+
   return (
-    <div className="bg-white flex flex-col items-center py-10">
+    <div className="w-full bg-white flex flex-col items-center py-10">
       <h2 className="text-2xl md:text-3xl font-semibold text-center text-black mb-4">
         What Our Customer Say About Us?
       </h2>
@@ -10,40 +48,39 @@ function Testimonials() {
         Our Curated Baskets make your work easy and affordable, combining top
         talent and the right.
       </p>
-      <div className="flex  md:flex-row flex-col justify-center gap-6 px-4 md:px-0">
-        <div className="bg-[#f3f3f3] border border-gray-200 rounded-3xl shadow p-6 w-full md:w-1/2">
-          <h4 className="text-lg font-bold text-black mb-2">
-            Ashutosh Pratap Singh
-          </h4>
-          <p className="text-sm text-gray-600 mb-4">
-            Founder, Technical Sapien (1M+ Followers)
-          </p>
-          <p className="text-gray-700 text-sm">
-            I am writing this recommendation to express my gratitude towards the
-            community management services provided by MetaupSpace to Technical
-            Sapien. As the founder of Technical Sapien, I am impressed by the
-            exceptional work done by your team. <br />
-            <br />
-            Not only that, but your team has also helped us manage our humongous
-            community of 500,000+ members, which we proudly refer to as
-            India&apos;s biggest tech-productive community.
-          </p>
-        </div>
-
-        <div className="bg-[#f3f3f3] border border-gray-200 rounded-3xl shadow p-6 w-full md:w-1/2">
-          <h4 className="text-lg font-bold text-black mb-2">Samarth Mahabia</h4>
-          <p className="text-sm text-gray-600 mb-4">
-            Youtuber, Barefox (150k+ Subscriber)
-          </p>
-          <p className="text-gray-700 text-sm">
-            Finding MetaUpSpace was a game-changer! Their editing services and
-            content strategy have propelled my channel to new heights. Their
-            professionalism, knowledge, and willingness to go the extra mile are
-            outstanding. If you&apos;re looking for a trustworthy partner in
-            your content creation journey, MetaUpSpace is the answer. Thanks for
-            being the best!
-          </p>
-        </div>
+      <div className="relative w-full">
+      <Swiper
+  spaceBetween={10}
+  loop={true}
+  speed={5000}
+  autoplay={{
+    delay: 2500,
+    disableOnInteraction: false,
+  }}
+ 
+  modules={[Autoplay]}
+  
+  className="mySwiper"
+  autoHeight={true}
+>
+  {testimonials.map((testimonial, index) => (
+    <SwiperSlide key={index}>
+      <div className="testimonial-slide bg-[#f3f3f3] border border-gray-200 rounded-3xl shadow p-6 w-full md:w-1/3 mx-auto h-auto">
+        <h4 className="text-lg font-bold text-black mb-2">
+          {testimonial.name}
+        </h4>
+        <p className="text-sm text-gray-600 mb-4">
+          {testimonial.designation}
+        </p>
+        <p className="text-gray-700 text-sm">
+          {testimonial.description}
+        </p>
+      </div>
+    </SwiperSlide>
+  ))}
+</Swiper>
+<div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-white dark:from-background z-50 "></div>
+<div className="pointer-events-none absolute inset-y-0 right-0 w-1/5 bg-gradient-to-l from-white dark:from-background z-50 "></div>
       </div>
     </div>
   );
