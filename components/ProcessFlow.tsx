2999e5882e75ec558e52e9da8dbb75e6b6c30de9
avnee-gy <avneegoyal410@gmail.com>
2024-08-24 15:07:54 +0530

    chore: proccess flow timeline changed

--- components/ProcessFlow.tsx
+++ components/ProcessFlow.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Image from "next/image";
+import timelineIcon from "../public/assets/node-js.png";
 
 const steps = [
   {
@@ -53,7 +54,7 @@ const steps = [
 
 const ProcessFlow = () => {
   return (
-    <section id="process" className="py-20 pt-40 bg-black text-gray-100">
+    <section id="process" className=" py-40  bg-black text-white">
       <div className="container mx-auto relative">
         <div className="text-center mb-10">
           <h1 className="text-4xl font-bold">Process Flow</h1>
@@ -63,46 +64,43 @@ const ProcessFlow = () => {
         </div>
         <div className="relative">
           <VerticalTimeline
-            layout={"2-columns"}
+            layout={"1-column-right"}
             animate={false}
-            lineColor={"white"}
+            lineColor={"#D7D7D7"}
           >
             {steps.map((step, index) => (
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
                 key={index}
-                contentStyle={{ background: "black" }}
+                contentStyle={{ background: "#D7D7D7" }}
+                // icon={timelineIcon}
               >
-                <h2 className={`text-xl font-bold pb-5 `}>{step.num}</h2>
-                <Image
-                  src={step.image}
-                  alt={step.title}
-                  height={200}
-                  width={200}
-                  className=""
-                />
-                <h3 className="text-lg font-bold mt-5">{step.title}</h3>
-                <p className="mt-2 ">{step.description}</p>
+                {/* <div className="absolute inset-x-0 top-0 h-20 bg-gradient-to-b from-[#19191A] to-transparent pointer-events-none"></div> */}
+
+                <h2 className="  vertical-timeline-element-title text-xl font-bold pb-5 text-white">
+                  {step.num}
+                </h2>
+                <div className="flex justify-between gap-5">
+                  <Image
+                    src={step.image}
+                    alt={step.title}
+                    height={150}
+                    width={150}
+                    className=""
+                  />
+                  <div>
+                    <h3 className="text-lg font-bold mt-5 text-black">
+                      {step.title}
+                    </h3>
+                    <p className="mt-2 text-black">{step.description}</p>
+                  </div>
+                </div>
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
-          {/* <div className="absolute inset-0 flex justify-center">
-            <div className="w-px bg-gray-300 h-full"></div>
-          </div>
-          {steps.map((step, index) => (
-            <div
-              key={step.key}
-              className={`flex ${
-                index % 2 === 0
-                  ? "justify-end text-left"
-                  : "justify-start text-right"
-              }`}
-            >
-              <StepCard step={step} index={index} />
-            </div>
-          ))} */}
         </div>
       </div>
+      {/* <div className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-t from-[#19191A] to-white pointer-events-none"></div> */}
     </section>
   );
 };
